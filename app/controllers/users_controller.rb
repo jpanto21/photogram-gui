8e560ba7b2c8defb0e1c9ab3385a012fb9ad61fb
Jose Pantoja <77706927+jpanto21@users.noreply.github.com>
2024-02-20 06:57:00 +0000

    final commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   
+
+  def home
+    redirect_to("/users")
+  end
   def index 
     matching_users = User.all
     @list_of_users = matching_users.order({ :username => :asc })
@@ -15,4 +19,30 @@ class UsersController < ApplicationController
     
     render({:template => "user_templates/show"})
   end
+
+  def update
+    url_username = params.fetch("modify_id")
+    matching_usernames = User.where({ :username => url_username})
+    the_username = matching_usernames.at(0)
+
+    input_username = params.fetch("query_username")
+
+    the_username.username = input_username
+
+    the_username.save
+
+    redirect_to("/users/"+ the_username.username)
+  end
+
+  def create 
+    input_username = params.fetch("query_username")
+    
+    a_new_user = User.new
+  
+    a_new_user.username = input_username
+    
+    a_new_user.save
+  
+    redirect_to("/users/"+ a_new_user.username)
+    end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "home"})
+
   get("/users", { :controller => "users", :action => "index"})
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
@@ -7,5 +9,17 @@ Rails.application.routes.draw do
   get("/photos", { :controller => "photos", :action => "index"})
 
   get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "delete"})
+
+  post("/insert_photo", { :controller => "photos", :action => "create"})
+
+  post("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
+
+  post("/insert_username", { :controller => "users", :action => "create"})
+
+  post("/update_username/:modify_id", { :controller => "users", :action => "update"})
+
+  post("/insert_comment", { :controller => "photos", :action => "insert"})
 
 end
